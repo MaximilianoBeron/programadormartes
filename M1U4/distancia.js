319cfef20f28ac627dae260e669a821e47b5fe2b
Maximiliano Berón <mberon@hcdn.gob.ar>
2022-09-12 19:58:32 -0300

    Modifiqué el archivo distancia.js (Ejercicio 1) y agregué el archivo mayor.js (Ejercicio 2)

--- M1U4/distancia.js
+++ M1U4/distancia.js
@@ -1,7 +1,10 @@
 const nombre = prompt('¡Hola! Ingrese su nombre: ', '');
 const distancia = parseInt(prompt('Ingrese la cantidad de metros que necesita recorrer:', ''));
+
 console.log(`El usuario se llama ${nombre} e ingresó ${distancia} metros`);
 
+
+    
 if (distancia <= 1000) {
     document.write(`Estimad@ ${nombre}, le conviene ir a pie.`);
     console.log(`Se le recomendó al usuario ir a pie.`)
@@ -21,4 +24,5 @@ if (distancia <= 1000) {
 } else if (distancia > 100000) {
     document.write(`Estimad@ ${nombre}, le conviene ir en avión.`);
     console.log(`Se le recomendó al usuario ir en avión.`)
+
 }
---
+++ M1U4/mayor.js
@@ -0,0 +1,21 @@
+const nombre = prompt('¡Hola! Ingrese nombre: ', '');
+const n1 = prompt('Ingrese el primer número: ', '');
+const n2 = prompt('Ingrese el segundo número: ', '');
+const n3 = prompt('Ingrese el tercer número: ', '');
+const n4 = prompt('Ingrese el cuerto número: ', '');
+const n5 = prompt('Ingrese el quinto número: ', '');
+
+
+const numeros = [n1, n2, n3, n4, n5];
+
+let mayor = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+        mayor = (numeros[i]);
+    }
+}
+
+document.write(`Estimado ${nombre}, el número mayor que ha ingresado es ${mayor}.`);
+console.log(`El número mayor del usuaraio ${nombre} es ${mayor}`);
+
